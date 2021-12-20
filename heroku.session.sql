357542809ef7a6077c7d1da30aef4d45dfdfc03c
Pedro Dos Santos <90657983+PLSR12@users.noreply.github.com>
2021-12-20 11:07:48 -0300

    Add files via upload

---
+++ heroku.session.sql
@@ -0,0 +1,17 @@
+--CREATE TABLE noticias(
+  -- id_noticia SERIAL PRIMARY KEY,
+  -- titulo VARCHAR(100) NOT NULL,
+  -- conteudo TEXT NOT NULL, 
+  -- data_criacao TIMESTAMP WITHOUT TIME ZONE DEFAULT(NOW()AT TIME ZONE'utc+3'))
+
+    -- INSERT INTO noticias (
+       --    titulo,
+       --    conteudo
+
+--VALUES(
+    --   'Notícia teste 4',
+    --   'quarta notícia com postgress')
+
+
+SELECT * FROM noticias
+
